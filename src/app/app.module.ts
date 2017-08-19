cb88c125333ff51e2e157d21844fe2af8f2f453a
Manzur Khan Sarguru <manzurkds@gmail.com>
2017-08-19 21:27:29 +0530

    cover complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OpportunityComponent } from './components/opportunity/opportunity.component';
+import { GeneralInfoComponent } from './components/opportunity/general-info/general-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OpportunityComponent,
+    GeneralInfoComponent
   ],
   imports: [
     BrowserModule
